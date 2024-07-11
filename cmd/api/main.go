1afc61e79f044d8f26444e2df7005b37bc4b73fe
Hüseyin Akbaş <huseyinakbasblog@gmail.com>
2024-07-11 14:03:01 +0300

    feat: init handlers

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -7,11 +7,29 @@ import (
 )
 
 func main() {
+	gin.SetMode(gin.ReleaseMode)
 	r := gin.Default()
-	r.GET("/", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-			"message": "Hello, world!",
-		})
-	})
+
+	r.POST("", CreateTodo)
+	r.GET("", ReadTodo)
+	r.PUT("", UpdateTodo)
+	r.DELETE("", DeleteTodo)
+
 	r.Run()
+}
+
+func CreateTodo(context *gin.Context) {
+	context.JSON(http.StatusOK, "Get Todo")
+}
+
+func ReadTodo(context *gin.Context) {
+	context.JSON(http.StatusOK, "Read Todo")
+}
+
+func UpdateTodo(context *gin.Context) {
+	context.JSON(http.StatusOK, "Update Todo")
+}
+
+func DeleteTodo(context *gin.Context) {
+	context.JSON(http.StatusOK, "Delete Todo")
 }
